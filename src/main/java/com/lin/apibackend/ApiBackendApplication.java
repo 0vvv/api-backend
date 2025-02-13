3a858944519c498ead6cb0179503b38dfc4508a2
0vvv <18301883265@163.com>
2025-02-13 22:14:20 +0800

    dubbo sample

--- src/main/java/com/lin/apibackend/ApiBackendApplication.java
+++ src/main/java/com/lin/apibackend/ApiBackendApplication.java
@@ -1,11 +1,13 @@
 package com.lin.apibackend;
 
+import org.apache.dubbo.config.spring.context.annotation.EnableDubbo;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @MapperScan("com.lin.apibackend.mapper")
 @SpringBootApplication(scanBasePackages = {"com.lin.apibackend", "com.lin.apiclientsdk"})
+@EnableDubbo
 public class ApiBackendApplication {
 
 	public static void main(String[] args) {
